e49c4a29d43dfbe79548650041c2b024382e7a47
Omas <omas.abdullah@gmail.com>
2020-09-13 00:07:11 -0400

    Established better naming convention

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from "react";
 import Meme from "./Meme";
 
 const MemeGenerator = (props) => {
-  const [topText, setTopText] = useState("");
-  const [bottomText, setBottomText] = useState("");
-  const [activeMemes, setActiveMemes] = useState([]);
-  const [memeDataSet, setMemeDataSet] = useState([]);
+  const [textFieldTop, setTextFieldTop] = useState("");
+  const [textFieldBottom, setTextFieldBottom] = useState("");
+  const [dataActiveMemes, setDataActiveMemes] = useState([]);
+  const [dataMemes, setDataMemes] = useState([]);
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((response) => response.json())
-      .then((response) => setMemeDataSet(response.data.memes));
+      .then((response) => setDataMemes(response.data.memes));
   }, []);
 
   const handleGenerateMeme = (event) => {
     const entries = [1, 2, 3].map((x) =>
-      Math.floor(Math.random() * memeDataSet.length)
+      Math.floor(Math.random() * dataMemes.length)
     );
-    setActiveMemes(entries);
+    setDataActiveMemes(entries);
   };
 
   return (
@@ -26,25 +26,25 @@ const MemeGenerator = (props) => {
         <input
           type="text"
           placeholder="Top Text"
-          onChange={(event) => setTopText(event.target.value)}
-          value={topText}
+          onChange={(event) => setTextFieldTop(event.target.value)}
+          value={textFieldTop}
         />
         <input
           type="text"
           placeholder="Bottom Text"
-          onChange={(event) => setBottomText(event.target.value)}
-          value={bottomText}
+          onChange={(event) => setTextFieldBottom(event.target.value)}
+          value={textFieldBottom}
         />
         <button type="button" onClick={handleGenerateMeme}>
           Generate Meme
         </button>
       </form>
-      {activeMemes.map((entry) => (
+      {dataActiveMemes.map((entry) => (
         <Meme
           key={entry}
-          meme={memeDataSet[entry]}
-          topText={topText}
-          bottomText={bottomText}
+          meme={dataMemes[entry]}
+          topText={textFieldTop}
+          bottomText={textFieldBottom}
         />
       ))}
     </div>
