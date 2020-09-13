18ffca48f57f2a3a76e05dfe32713a0f10d90163
Omas <omas.abdullah@gmail.com>
2020-09-13 00:21:40 -0400

    Added styling for text

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styles from "./mystyle.module.css";
 
 const Meme = (props) => {
   return (
-    <div>
-      <h2> {props.topText} </h2>
-      <h2> {props.bottomText} </h2>
-      <img src={props.meme.url} alt="" height="240px"/>
+    <div className={styles.container}>
+      <img
+        src={props.meme.url}
+        alt=""
+        height="240px"
+      />
+      <h2 className={styles.topText}> {props.topText} </h2>
+      <h2 className={styles.bottomText}> {props.bottomText} </h2>
     </div>
   );
 };
