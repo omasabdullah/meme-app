70e928eb4b22afa5de643a876974658f2509276d
Omas <omas.abdullah@gmail.com>
2020-09-13 00:01:52 -0400

    Render memes in main return function

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Meme from './Meme'
+import Meme from "./Meme";
 
 const MemeGenerator = (props) => {
   const [topText, setTopText] = useState("");
@@ -9,48 +9,44 @@ const MemeGenerator = (props) => {
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
-      .then(response => response.json())
-      .then(response => setMemeDataSet(response.data.memes));
+      .then((response) => response.json())
+      .then((response) => setMemeDataSet(response.data.memes));
   }, []);
 
   const handleGenerateMeme = (event) => {
-    const entries = [1, 2, 3].map(x => Math.floor(Math.random()*memeDataSet.length));
-    setActiveMemes(entries);
-  }
-
-  const MemeList = () => {
-    return (
-      <div>
-        {activeMemes.map(entry => {
-          console.log(memeDataSet[entry]);
-          return (
-            <Meme
-              key={entry}
-              meme={memeDataSet[entry]}
-              topText={topText}
-              bottomText={bottomText} />
-          );
-        })}
-      </div>
+    const entries = [1, 2, 3].map((x) =>
+      Math.floor(Math.random() * memeDataSet.length)
     );
-  }
+    setActiveMemes(entries);
+  };
 
   return (
     <div>
       <form>
         <input
-          type='text'
-          placeholder='Top Text'
+          type="text"
+          placeholder="Top Text"
           onChange={(event) => setTopText(event.target.value)}
-          value={topText} />
+          value={topText}
+        />
         <input
-          type='text'
-          placeholder='Bottom Text'
+          type="text"
+          placeholder="Bottom Text"
           onChange={(event) => setBottomText(event.target.value)}
-          value={bottomText} />
-        <button type='button' onClick={handleGenerateMeme}>Generate Meme</button>
+          value={bottomText}
+        />
+        <button type="button" onClick={handleGenerateMeme}>
+          Generate Meme
+        </button>
       </form>
-      {MemeList()}
+      {activeMemes.map((entry) => (
+        <Meme
+          key={entry}
+          meme={memeDataSet[entry]}
+          topText={topText}
+          bottomText={bottomText}
+        />
+      ))}
     </div>
   );
 };
